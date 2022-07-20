047891e46177614a6d346a10953acafd18d48bec
Reza Nazari <rz.nzrisg@gmail.com>
2022-07-20 18:59:59 +0430

    fix: first run on device

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import store from './src/store';
 import { getConfigurations } from './src/store/slices/appSlice';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { RootStack } from './src/routes/RootStack';
 
 // Set the locale once at the beginning of your app.
 import './src/assets/dictionary';
